d4874ecda3dc62f872ed6569f129f4c142d1a217
Andrea010203 <157354411+Andrea010203@users.noreply.github.com>
2024-01-24 15:57:39 +0000

    mmmerda

--- numeroprimo.c
+++ numeroprimo.c
@@ -1,1 +1,31 @@
- 
+    #include <stdio.h>
+
+        int main() 
+{
+        int numero, somma = 0, contatore = 0;
+
+     printf("Inserisci una sequenza di numeri interi positivi. Inserisci un numero negativo per terminare.\n");
+
+        while (1) 
+    {
+        printf("Inserisci un numero: ");
+        scanf("%d", &numero);
+
+        
+        if (numero < 0) {
+            break;
+        }
+
+        
+        somma += numero;
+        contatore++;
+
+        
+        float media = (float)somma / contatore;
+        printf("Media attuale: %.2f\n", media);
+    }
+
+    printf("Programma terminato.\n");
+
+    return 0;
+}
